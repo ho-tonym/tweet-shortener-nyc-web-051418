ea94184783a186daab4a71485b027cf209ade14e
Tony Ho <ho.tonym@gmail.com>
2018-05-10 16:08:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,7 @@ def word_substituter(long_string)
     array_strings.each_with_index do |word, index|
       @dictionary.each do |key, value|
         if word == key
-          array_strings[index] = @dictionary[key]
+          array_strings[index] = dictionary[key]
         end
       end
     end
