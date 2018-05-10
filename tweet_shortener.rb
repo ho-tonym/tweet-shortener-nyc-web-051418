5f5274e5f8491f8967d1d3793a6f4f8a1cbe6ab8
Tony Ho <ho.tonym@gmail.com>
2018-05-10 15:21:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require "pry"
+
+
+
+def word_substituter(long_string)
+  dictionary = {
+  "hello" => 'hi',
+  "to, two, too" => '2',
+  "for, four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+  }
+
+keys = dictionary.keys
+
+  array_strings = long_string.split(" ")
+    array_strings.each do |word|
+      keys.each do |key|
+        if word == key
+          word = dictionary[key]
+          binding.pry
+        end
+      end
+    end
+
+array_strings.join(" ")
+
+end
+
+word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
