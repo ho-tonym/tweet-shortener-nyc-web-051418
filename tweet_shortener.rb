91b9b46ae1ea28daf5c7726733868097a6b678c4
Tony Ho <ho.tonym@gmail.com>
2018-05-10 15:36:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ def word_substituter(long_string)
     array_strings.each do |word|
       dictionary.each do |key, value|
         if word == key
-          array_strings.gsub!(/word/, dictionary[key])
+          array_strings.gsub!(word, dictionary[key])
         end
       end
     end
