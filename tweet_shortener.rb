7152e0ddb1ecb08e4f29906bff4f6fa58105c603
Tony Ho <ho.tonym@gmail.com>
2018-05-10 16:08:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,26 +1,26 @@
 require "pry"
 
-
+@dictionary = {
+"hello" => 'hi',
+"to" => '2',
+"two"=> '2',
+"too"=> '2',
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
+}
 
 def word_substituter(long_string)
-  dictionary = {
-  "hello" => 'hi',
-  "to" => '2',
-  "two"=> '2',
-  "too"=> '2',
-  "for" => '4',
-  "four" => '4',
-  'be' => 'b',
-  'you' => 'u',
-  "at" => "@",
-  "and" => "&"
-  }
+
 
   array_strings = long_string.split(" ")
     array_strings.each_with_index do |word, index|
-      dictionary.each do |key, value|
+      @dictionary.each do |key, value|
         if word == key
-          array_strings[index] = dictionary[key]
+          array_strings[index] = @dictionary[key]
         end
       end
     end
@@ -30,3 +30,5 @@ def word_substituter(long_string)
 end
 
 word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+
+bulk
