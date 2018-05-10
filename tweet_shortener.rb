cc07f859346c587faba8a2188ff6ab5d044c674f
Tony Ho <ho.tonym@gmail.com>
2018-05-10 18:58:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,9 +40,9 @@ end
 
 def selective_tweet_shortener(tweet)
   #shorten the passed in tweet
-  tweet.length > 140 ? "#{tweet[0...140]}..." : tweet
+  tweet.length > 140 ? "#{tweet[0...140]}" : tweet
 end
 
 def shortened_tweet_truncator(tweet)
-  tweet.length > 140 ? "#{tweet[0...140]}..." : tweet
+  tweet.length > 140 ? "#{tweet[0...140]}" : tweet
 end
